29469862e72d7f739d770181f920a2c1737addc5
Viktor Sharha <vsharha07@gmail.com>
2026-05-23 17:59:30 +0100

    Add Windows install script and initial app list

---
+++ windows/bootstrap.ps1
@@ -0,0 +1,16 @@
+# Wait for network connectivity
+Write-Host "Waiting for network..."
+while (-not (Test-NetConnection -ComputerName 8.8.8.8 -InformationLevel Quiet)) {
+    Start-Sleep -Seconds 5
+}
+
+# Fetch and import app list
+Write-Host "Installing apps..."
+$appsUrl = "https://raw.githubusercontent.com/vsharha/dotfiles/main/windows/apps.json"
+$appsJson = (Invoke-WebRequest -Uri $appsUrl -UseBasicParsing).Content
+$tmpFile = "$env:TEMP\apps.json"
+$appsJson | Out-File -FilePath $tmpFile -Encoding utf8
+
+winget import --import-file $tmpFile --ignore-unavailable --no-upgrade
+
+Write-Host "Done."
